2915930acabeecbc4dea240b3ba7fd4d9eee3a3b
Sam (Xemor) <samuelhollis@outlook.com>
2025-02-14 13:50:47 +0000

    comment out more

--- src/types.rs
+++ src/types.rs
@@ -24,19 +24,19 @@ pub struct Ball {
     pub links: Vec<Link>,
 }
 
-impl Serialize for Ball {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let formatted_date = self.date.format("%B %e %Y").to_string();
-        let mut state = serializer.serialize_struct("Ball", 6)?;
-        state.serialize_field("name", &self.name)?;
-        state.serialize_field("date", &formatted_date)?;
-        state.serialize_field("theme", &self.theme)?;
-        state.serialize_field("price", &self.price)?;
-        state.serialize_field("season", &self.season)?;
-        state.serialize_field("links", &self.links)?;
-        state.end()
-    }
-}
+// impl Serialize for Ball {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer,
+//     {
+//         let formatted_date = self.date.format("%B %e %Y").to_string();
+//         let mut state = serializer.serialize_struct("Ball", 6)?;
+//         state.serialize_field("name", &self.name)?;
+//         state.serialize_field("date", &formatted_date)?;
+//         state.serialize_field("theme", &self.theme)?;
+//         state.serialize_field("price", &self.price)?;
+//         state.serialize_field("season", &self.season)?;
+//         state.serialize_field("links", &self.links)?;
+//         state.end()
+//     }
+// }
